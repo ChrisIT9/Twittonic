1a41a934be8d3b43b1e5faae3008817a9fff4b57
Christian <cri-9999@live.it>
2022-02-06 03:32:46 +0100

    deleted redundant files from repo

--- twittonic_backend/build/middlewares/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkAuthHeaders = void 0;
-var checkAuthHeaders = function (req, res, next) {
-    if ('authorization' in req.headers)
-        return next();
-    return res.status(400).json({ message: "Token is missing!" });
-};
-exports.checkAuthHeaders = checkAuthHeaders;
